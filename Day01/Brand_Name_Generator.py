520cf1b67fc03ad19894a5a3d5b820ea299f02ea
Ganesh Ghube <ganesh.ghube@gmail.com>
2024-10-17 18:31:39 +0100

    Update

---
+++ Day01/Brand_Name_Generator.py
@@ -0,0 +1,8 @@
+#Ask user for input
+print("Welcome to The Band Name Generator!")
+#Input variable where user can type string
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your first pet?\n")
+colour = input("What is your faviouriate colour?\n")
+#Print user input city and pet
+print("Your band name is " + city +  " " + pet + " " + colour)
